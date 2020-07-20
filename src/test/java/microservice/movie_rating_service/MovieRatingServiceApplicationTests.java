10c5d5137c4d3b703c51749dcc684c651a801ba6
BartnikLukasz <bartnik.luk@gmail.com>
2020-07-20 12:03:59 +0200

    First commit

---
+++ src/test/java/microservice/movie_rating_service/MovieRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.movie_rating_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
