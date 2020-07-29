eb3e4444078d686270a3fe2b413909b231d4d7c1
BartnikLukasz <bartnik.luk@gmail.com>
2020-07-29 10:41:32 +0200

    Added comments

--- src/main/java/microservice/movie_rating_service/MainController.java
+++ src/main/java/microservice/movie_rating_service/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+//This controller is handling requests sent to it by movie data microservice
 @Controller
 @RequestMapping(path="/ratingservice")
 public class MainController {
